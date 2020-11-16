429d4bd863ef83093612820d0a030854895efaca
Tomas Poveda <tpovedatd@gmail.com>
2020-11-16 14:04:57 +0100

    Added loader module

---
+++ tpDcc/libs/resources/loader.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Initialization module for tpDcc-libs-resources
+"""
+
+import os
+
+
+def init(*args, **kwargs):
+    """
+    Initializes library
+    """
+
+    # Import resources
+    from tpDcc.libs.resources import res
