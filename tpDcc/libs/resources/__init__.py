ea75541dc3ae463301db9c7fcc5e1ebaf4b787fb
Tomas Poveda <tpovedatd@gmail.com>
2020-11-26 00:38:29 +0100

    Moved resources library definition

--- tpDcc/libs/resources/__init__.py
+++ tpDcc/libs/resources/__init__.py
@@ -11,6 +11,30 @@ import os
 import logging.config
 
 
+LIB_ID = 'tpDcc-libs-resources'
+LIB_ENV = LIB_ID.replace('-', '_').upper()
+
+from tpDcc.core import library
+
+
+class ResourcesLib(library.DccLibrary, object):
+    def __init__(self, *args, **kwargs):
+        super(ResourcesLib, self).__init__(*args, **kwargs)
+
+    @classmethod
+    def config_dict(cls, file_name=None):
+        base_tool_config = library.DccLibrary.config_dict(file_name=file_name)
+        tool_config = {
+            'name': 'Resources Library',
+            'id': LIB_ID,
+            'supported_dccs': {'maya': ['2017', '2018', '2019', '2020']},
+            'tooltip': 'Library to manage resources'
+        }
+        base_tool_config.update(tool_config)
+
+        return base_tool_config
+
+
 def create_logger(dev=False):
     """
     Creates logger for current tpDcc-libs-resources package
